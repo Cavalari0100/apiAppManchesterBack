e11ecb1f1a66f21178a340a9dd72694ee106ce55
Cavalari0100 <joao.cavalarispa@hotmail.com>
2021-09-30 12:00:06 -0300

    mudando o cors

--- config/customExpress.js
+++ config/customExpress.js
@@ -15,7 +15,7 @@ module.exports = () => { //module.exports é como se fosse uma lib em outras lin
 
 
     app.use(function (req, res, next) {
-      res.header("Access-Control-Allow-Origin", "https://appmanchesterfront.vercel.app/");
+      res.header("Access-Control-Allow-Origin", "*" );
       res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE,PATCH");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
       next();
